18c8fdeb3f558e196c1583c7d232ab25af43caab
Thomas Pope <tpope0928@gmail.com>
2018-09-07 13:43:11 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -26,8 +26,10 @@ class Owner
     "I am a #{species}."
   end
   
-  
-  
+  def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
+  end
+
   def buy_cat
     pets[:cats] << Cats.new(name)
   end
